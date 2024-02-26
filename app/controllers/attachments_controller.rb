6353a7f0428e476505cbaf9815bcf6fd7d5207d6
Dubovtsov <dubwin@yandex.ru>
2024-02-26 18:20:20 +0300

    images

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,7 +2,24 @@ class AttachmentsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
+    # @file = current_user.files.find(params[:id])
     @file = ActiveStorage::Attachment.find(params[:id])
-    @file.purge
+    if @file.destroy
+      respond_to do |format|
+        # format.html { redirect_to :back, notice: 'File was successfully destroyed.' }
+        # format.json { head :no_content }
+        format.turbo_stream { turbo_stream.remove(@file) }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to :back, alert: 'File could not be destroyed.' }
+        format.json { render json: @file.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # def destroy
+  #   @file = ActiveStorage::Attachment.find(params[:id])
+  #   @file.purge
+  # end
 end
