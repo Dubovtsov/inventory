fd3e26e6f92d8543fb9a9d657d7d63bdc09bccbf
Dubovtsov <dubwin@yandex.ru>
2024-04-05 17:42:59 +0300

    test

--- test/models/price_test.rb
+++ test/models/price_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: prices
+#
+#  id             :bigint           not null, primary key
+#  item_number    :string
+#  purchase_price :decimal(, )
+#  retail_price   :decimal(, )
+#  title          :string
+#  vat            :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  vendor_id      :bigint           not null
+#
+# Indexes
+#
+#  index_prices_on_vendor_id  (vendor_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (vendor_id => vendors.id)
+#
 require "test_helper"
 
 class PriceTest < ActiveSupport::TestCase
--- test/models/storehouse_test.rb
+++ test/models/storehouse_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: storehouses
+#
+#  id          :bigint           not null, primary key
+#  deleted_at  :datetime
+#  description :text
+#  employee    :boolean          default(FALSE)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_storehouses_on_deleted_at  (deleted_at)
+#
 require "test_helper"
 
 class StorehouseTest < ActiveSupport::TestCase
